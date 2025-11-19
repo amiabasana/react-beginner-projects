4d1f82b281100cf04cd13758ef33c3e8a08b7c19
Amisha Abasana <abasanaamisha2001@gmail.com>
2025-11-19 19:50:12 +0530

    feat: add react-interview-projects
    feat: add react-interview-projects
    create accordion

---
+++ REACT-INTERVIEW-PROJECTS/src/components/accordian/data.js
@@ -0,0 +1,24 @@
+  const data = [
+    {
+      id: "1",
+      title: "Javascript basics",
+      content: "Learn variables, functions, and loops in JavaScript.",
+    },
+    {
+      id: "2",
+      title: "React.js Overview",
+      content: "Understand components, state, and props in React.",
+    },
+    {
+      id: "3",
+      title: "Node.js",
+      content: "Basics of server-side development with Node.js.",
+    },
+    {
+      id: "4",
+      title: "Full-Stack Development",
+      content: "Build full-stack apps with React and Node.js.",
+    },
+  ];
+
+export default data;
